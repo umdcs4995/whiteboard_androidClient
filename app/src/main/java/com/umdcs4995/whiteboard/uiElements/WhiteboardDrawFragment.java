50092c15b7b9d4963c09cd27955cf59020abac95
Kade Helseth <helse014@d.umn.edu>
2016-03-24 12:02:31 -0500

    Added a revert button

--- app/src/main/java/com/umdcs4995/whiteboard/uiElements/WhiteboardDrawFragment.java
+++ app/src/main/java/com/umdcs4995/whiteboard/uiElements/WhiteboardDrawFragment.java
@@ -53,10 +53,8 @@ public class WhiteboardDrawFragment extends Fragment implements View.OnClickList
 
 
     //initialize brush sizes
+    //TODO grab from the resource file
     private float smallBrush = 5, mediumBrush = 10, largeBrush = 15;
-
-    //option menus for the buttons and paints
-    private LinearLayout OptionButtons, PaintOptions;
 
 
     //Camera Window
@@ -82,20 +80,6 @@ public class WhiteboardDrawFragment extends Fragment implements View.OnClickList
         //set up the Drawing view
         drawView.setupDrawing();
         }
-
-//TODO look at and revise if needed
-    /**
-     * calls on the gesture motion detection to be used when the application is touched
-     * @param event the touch event happening
-     * @return if the touch was handled
-     */
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event){
-//        this.masterDetector.onTouchEvent(event);
-//        // Be sure to call the superclass implementation
-//        return getActivity().onTouchEvent(event);
-//    }
-
 
     /**
      * Sets a the brush color when a paint color is selected to the input view's
@@ -136,6 +120,9 @@ public class WhiteboardDrawFragment extends Fragment implements View.OnClickList
         button = (ImageButton) getActivity().findViewById(R.id.save_btn);
         button.setOnClickListener(this);
 
+        button = (ImageButton) getActivity().findViewById(R.id.back_btn);
+        button.setOnClickListener(this);
+
         button = (ImageButton) getActivity().findViewById(R.id.btn_drawfrag_color1);
         button.setOnClickListener(this);
         button = (ImageButton) getActivity().findViewById(R.id.btn_drawfrag_color2);
@@ -160,7 +147,6 @@ public class WhiteboardDrawFragment extends Fragment implements View.OnClickList
         button.setOnClickListener(this);
         button = (ImageButton) getActivity().findViewById(R.id.btn_drawfrag_color12);
         button.setOnClickListener(this);
-
 
     }
 
