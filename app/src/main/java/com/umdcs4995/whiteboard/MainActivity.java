826b1dda9a29ebcf3416da23bd4080df7738bda3
Laura Krebs <krebs148@d.umn.edu>
2016-03-29 11:58:09 -0500

    added google-services.json file and configuration in Gradle for OAuth and Drive support

--- app/src/main/java/com/umdcs4995/whiteboard/MainActivity.java
+++ app/src/main/java/com/umdcs4995/whiteboard/MainActivity.java
@@ -4,20 +4,19 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.umdcs4995.whiteboard.services.SocketService;
@@ -187,6 +186,11 @@ public class MainActivity extends AppCompatActivity
        tempFragment.setNewBackground(urlString);
         changeMainFragment(whiteboardDrawFragment);
 
+    }
+
+    public void onLoginBtnClicked() {
+        WhiteboardDrawFragment tempFragment = (WhiteboardDrawFragment) whiteboardDrawFragment;
+        //tempFragment.
     }
 
     @Override
